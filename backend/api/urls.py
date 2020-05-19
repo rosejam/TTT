f2c543b4624d4ebe56fba364858d595dff26ff00
yury <seoyury@gmail.com>
2020-05-19 16:59:17 +0900

    20/05/19 | API 문서 자동화 drf-yasg 추가 | 서유리

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,28 @@
 from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
 from api import views
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 
 router = DefaultRouter(trailing_slash=False)
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
-urlpatterns = router.urls
+urlpatterns = [
+   url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+   ...
+]
+
+# urlpatterns = router.urls
