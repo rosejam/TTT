2ad737906ba209a458e7bc347966a44bebb3cf64
yury <seoyury@gmail.com>
2020-05-25 17:59:59 +0900

    20/05/25 | Window API TEST 중 | 서유리

--- windows_server/systrader/bridge_flask.py
+++ windows_server/systrader/bridge_flask.py
@@ -106,6 +106,19 @@ def handle_short():
     shorts = c.get_shortstockselling(stockcode, n=n)
     return jsonify(shorts)
 
+"""
+여기부터 유리 test 중
+"""
+@app.route('/codeandprice', methods=['GET'])
+def handle_stockcodeandprice():
+    c.wait()
+    market = request.args.get('market')
+    if market == 'kospi':
+        return jsonify(c.get_stockcodes(constants.MARKET_CODE_KOSPI))
+    elif market == 'kosdaq':
+        return jsonify(c.get_stockcodes(constants.MARKET_CODE_KOSDAQ))
+    else:
+        return '"codeandprice" should be one of "kospi" and "kosdaq".', 400
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
