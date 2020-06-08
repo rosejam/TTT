834d65a23ec1e94591d92c27aab7f204e7ae2b20
이은규 <a9871514@gmail.com>
2020-06-08 16:17:18 +0900

    20/06/08 | 차트 작업 및 모바일 최적화 | 이은규

--- frontend/src/components/Charts/UpDown.vue
+++ frontend/src/components/Charts/UpDown.vue
@@ -1,18 +1,55 @@
 <template>
-  <div class="row">
-    <div class="col-2">
-      시작 금액: 
-    </div>
-    <div class="col-3 text-left">
-      [{{data[0][1] | currency}}원]
-    </div>
-    <div class="col-2">
-      종료 금액: 
-    </div>
-    <div class="col-5 text-left">
-      [{{data[data.length-1][1] | currency}}원]<br/>
-      <div v-html="html"></div>
-    </div>
+  <div class="table-container">
+    <br/>
+    <table class="table">
+
+      <!-- 테이블 column 이름 -->
+      <thead>
+        <tr>
+          <td><strong>구분</strong></td>
+          <td><strong>금액</strong></td>
+          <td><strong>날짜</strong></td>
+        </tr>
+      </thead>
+
+      <!-- 테이블 내용 -->
+      <tbody>
+
+        <tr>
+          <td>시작 금액</td>
+          <td>{{data[0][1] | currency}} 원</td>
+          <td>{{highAndLow.startDay}}</td>
+        </tr>
+
+        <tr>
+          <td>최고 금액</td>
+          <td>
+            {{highAndLow.highAmount | currency}} 원
+            <div v-html="html.high"></div>
+          </td>
+          <td>{{highAndLow.highDay}}</td>
+        </tr>
+
+        <tr>
+          <td>최저 금액</td>
+          <td>
+            {{highAndLow.lowAmount | currency}} 원
+            <div v-html="html.low"></div>
+          </td>
+          <td>{{highAndLow.lowDay}}</td>
+        </tr>
+
+        <tr>
+          <td>종료 금액</td>
+          <td>
+            {{data[data.length-1][1] | currency}} 원
+            <div v-html="html.end"></div>
+          </td>
+          <td>{{highAndLow.endDay}}</td>
+        </tr>
+
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -31,13 +68,28 @@ export default {
   computed: {
     html() {
       return this.upDown();
-    }
+    },
+    highAndLow() {
+      return this.calcHL();
+    },
   },
   methods: {
+
     // 자산 변화 비율
     upDown() {
+      const end = this.calcUpDown(this.data[this.data.length-1][1]);
+      const high = this.calcUpDown(this.highAndLow.highAmount);
+      const low = this.calcUpDown(this.highAndLow.lowAmount);
+
+      return {
+        end: end,
+        high: high,
+        low: low
+      };
+    },
+
+    calcUpDown(endPrice) {
       const startPrice = this.data[0][1];
-      const endPrice = this.data[this.data.length-1][1];
       const ratio = ((endPrice - startPrice) / startPrice * 100).toFixed(4);
 
       let prefix = '+';
@@ -62,7 +114,49 @@ export default {
       return html;
     },
 
+    // 최고가, 최저가 계산
+    calcHL() {
 
+      let highDay, highAmount = 0, lowDay, lowAmount = Number.MAX_VALUE;
+
+      for (let i = 0; i < this.data.length; i++) {
+        const amount = this.data[i][1];
+
+        if(amount > highAmount) {
+          highAmount = amount;
+          highDay = this.data[i][0];
+        }
+
+        if(amount < lowAmount) {
+          lowAmount = amount;
+          lowDay = this.data[i][0];
+        }
+
+      }
+
+      let date = new Date(highDay);
+      highDay = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+
+      date = new Date(lowDay);
+      lowDay = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+
+      date = new Date(this.data[0][0]);
+      const startDay = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+
+      date = new Date(this.data[this.data.length-1][0]);
+      const endDay = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+
+      return {
+        highDay: highDay,
+        highAmount: highAmount,
+        lowDay: lowDay,
+        lowAmount: lowAmount,
+        startDay: startDay,
+        endDay: endDay,
+      };
+
+    }
+    
   },
 
   filters: {
